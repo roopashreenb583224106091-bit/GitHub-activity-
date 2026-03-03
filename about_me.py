887ad5d2a87ab219805bae2da419e6932fe9312c
roopashreenb583224106091-bit <roopashreenb583224106091@nprcolleges.org>
2026-03-03 18:50:08 +0530

    Create about_me.py

---
+++ about_me.py
@@ -0,0 +1,10 @@
+name = "Roopashree"
+course = "Electronics and Communication Engineering"
+language = "Python"
+
+print("Hello! 👋")
+print("My name is", name)
+print("I am studying", course)
+print("I am learning", language, "programming")
+print("This is my first GitHub repository.")
+print("Thank you for visiting! 🚀")
